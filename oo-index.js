82e2f106e366dd12053b50d7423b355d0ae4db96
NMoore42 <nicholas.moore5@icloud.com>
2019-05-21 09:59:57 -0500

    Done.

--- oo-index.js
+++ oo-index.js
@@ -1,3 +1,12 @@
+const eventArea = document.getElementById('event_area')
+const imageArea = document.getElementById('image_area')
+const restartButton = document.getElementById('restart').addEventListener("click",()=> newUni.restart())
+const attackButton = document.getElementById('attack').addEventListener("click",()=> newUni.attack())
+const defendButton = document.getElementById('defend').addEventListener("click",()=> newUni.defend())
+const healButton = document.getElementById('heal').addEventListener("click",()=> newUni.heal())
+const breakdanceButton = document.getElementById('breakdance').addEventListener("click",()=> newUni.breakdance())
+const eatButton = document.getElementById('eat').addEventListener("click",()=> newUni.eatCronut())
+
 class Unicorn {
   constructor(name, occupation) {
     this.name = name;
@@ -5,4 +14,74 @@ class Unicorn {
     this.health = 1000
   }
 
+  restart() {
+    while (eventArea.hasChildNodes()) {
+        eventArea.removeChild(eventArea.lastChild);
+    }
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("Out on the street, ready to go!"));
+    eventArea.prepend(div);
+    imageArea.className = 'new';
+    this.health = 1000;
+
+  }
+
+  attack() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
+    eventArea.prepend(div);
+    imageArea.className = 'attack';
+    this.health -= 50;
+    if (this.health <= 0) {
+      alert("You have died and shamed your family :(")
+    }
+  }
+
+  defend() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You put sunglasses on."));
+    eventArea.prepend(div);
+    imageArea.className = 'defend';
+    this.health -= 150;
+    if (this.health <= 0) {
+      alert("You have died and shamed your family :(")
+    }
+  }
+
+  heal() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You feel refreshed after yoga."));
+    eventArea.prepend(div);
+    imageArea.className = 'heal';
+    this.health += 500;
+  }
+
+  breakdance() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("It's really spinning class"));
+    eventArea.prepend(div);
+    imageArea.className = 'breakdance';
+    this.health -= 10;
+    if (this.health <= 0) {
+      alert("You have died and shamed your family :(")
+    }
+  }
+
+  eatCronut() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You've gained 1000 health points."));
+    eventArea.prepend(div);
+    imageArea.className = 'eat';
+    this.health += 1000;
+  }
+
 }
+
+let newUni = new Unicorn("Unk-Unk", "Plumber")
+
+
+//
+// createNew.addEventListener("click", function() {
+//   let newUni = new Unicorn("Unk-Unk", "Plumber")
+//   console.log(newUni)
+// })
